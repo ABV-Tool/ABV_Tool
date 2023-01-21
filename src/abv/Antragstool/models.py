f7c025cec08293be6c4ed65b22d48a1499d2f409
Göran Heinemann <goeran@karsdorf.net>
2023-01-21 12:36:54 +0100

    Modell für Antrag, ohne foreign keys

--- src/abv/Antragstool/models.py
+++ src/abv/Antragstool/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+
+class Antrag(models.Model):
+    antragID = models.UUIDField(primary_key=True,
+                                default=uuid.uuid4,
+                                editable=False)
+    sitzID = models.UUIDField()
+    typID = models.UUIDField()
+    astellerID = models.UUIDField()
+    antragText = models.TextField()
+    antragSumme = MoneyField(default_currency='EUR')
