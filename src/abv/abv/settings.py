af9f05b2350cf4885df6f1087b78f9b0eca07f72
Göran Heinemann <goeran@karsdorf.net>
2023-01-21 12:28:36 +0100

    datenbank credentials zum testen für docker container

--- src/abv/abv/settings.py
+++ src/abv/abv/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'abv.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abv',
+        'USER': 'abv',
+        'PASSWORD': 'abv',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
