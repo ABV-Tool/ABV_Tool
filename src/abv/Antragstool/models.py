811821d60fcb24a07aa96703a0f7d53adbfbe5b8
Göran Heinemann <goeran@karsdorf.net>
2023-01-21 12:44:02 +0100

    Sitzung hinzugefügt + fk in Antrag

--- src/abv/Antragstool/models.py
+++ src/abv/Antragstool/models.py
@@ -3,11 +3,19 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 
 
+class Sitzung(models.Model):
+    sitzID = models.UUIDField(primary_key=True,
+                              default=uuid.uuid4,
+                              editable=False)
+    refID = models.UUIDField()
+    sitzDate = models.DateField()
+
+
 class Antrag(models.Model):
     antragID = models.UUIDField(primary_key=True,
                                 default=uuid.uuid4,
                                 editable=False)
-    sitzID = models.UUIDField()
+    sitzID = models.ForeignKey(Sitzung)
     typID = models.UUIDField()
     astellerID = models.UUIDField()
     antragText = models.TextField()
