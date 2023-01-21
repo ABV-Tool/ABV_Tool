b2dd94653ae3d546a27caa095f7fffc9f9c9d14c
Göran Heinemann <goeran@karsdorf.net>
2023-01-21 15:48:49 +0100

    model funktioniert im adminbereich bis auf Money typ in Antrag

--- src/abv/Antragstool/admin.py
+++ src/abv/Antragstool/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Antrag, Sitzung, Referat, Antragssteller, Antragstyp
 
 # Register your models here.
+admin.site.register(Antrag)
+admin.site.register(Sitzung)
+admin.site.register(Referat)
+admin.site.register(Antragstyp)
+admin.site.register(Antragssteller)
