584c0af3c7c9333fe348f42e866b6b1a17ad69b6
MaxPtg <max.patecky@htw-dresden.de>
2023-04-16 17:57:45 +0200

    updated login form, added fa-icons (#24)

--- src/abv/abv/urls.py
+++ src/abv/abv/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('Antragstool.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
