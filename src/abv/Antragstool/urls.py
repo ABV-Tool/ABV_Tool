0be432df322ecdb1fbea0cdb7b20ec9cfb131a46
MaxPtg <max.patecky@htw-dresden.de>
2023-04-29 19:58:41 +0200

    small fixes

--- src/abv/Antragstool/urls.py
+++ src/abv/Antragstool/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import AppHome, LoginPage, LogoutPage, ReferatListe, SitzungenVonReferat, AntraegeVonSitzung
-from .views import AntragAllgemein, AntragFinanziell, AntragMitglied, AntragAmt, AntragBenehmen
+from .views import AntragAllgemein, AntragFinanziell, AntragVeranstaltung, AntragMitglied, AntragAmt, AntragBenehmen
 
 
 urlpatterns = [
     path('', AppHome, name='index'),
     
-    path('referate', ReferatListe, name='referate'),
+    path('referate/', ReferatListe, name='referate'),
     path('sitzungen/<uuid:sitzid>/', SitzungenVonReferat, name='SitzungenVonReferat'),
     path('sitzung/<uuid:sitzid>/', AntraegeVonSitzung, name='AntraegeVonSitzung'),
     
@@ -15,6 +15,7 @@ urlpatterns = [
     
     path('antrag/allgemein/', AntragAllgemein, name='antrag-allgemein'),
     path('antrag/finanziell/', AntragFinanziell, name='antrag-finanziell'),
+    path('antrag/veranstaltung/', AntragVeranstaltung, name='antrag-veranstaltung'),
     path('antrag/mitglied/', AntragMitglied, name='antrag-mitglied'),
     path('antrag/amt/', AntragAmt, name='antrag-amt'),
     path('antrag/benehmen/', AntragBenehmen, name='antrag-benehmen'),
