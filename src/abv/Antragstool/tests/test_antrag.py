48d2bbcd34ca3ed6b5702eaf5a8d581eda188dbe
MaxPtg <max.patecky@htw-dresden.de>
2023-06-04 17:14:01 +0200

    updated antrag tests

--- src/abv/Antragstool/tests/test_antrag.py
+++ src/abv/Antragstool/tests/test_antrag.py
@@ -1,39 +1,88 @@
-from Antragstool.models import Antrag
 import unittest
 from django.test import Client
 
+from Antragstool.models import Antrag
 
 
 class AntragAllgemeinTestCase(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.vorname = "Reiner"
+        self.nachname = "Zufall"
+        self.email = "reiner@zufall.de"
+        self.titel = "Test"
+        self.text = "Dies ist ein Testantrag"
+        self.grund = "Testgrund"
+        self.vorschlag = "Testvorschlag"
     
-    def test_antrag_allgemein(self):
+    def test_antrag_allgemein_erstellen(self):
         response = self.client.post('/antrag/allgemein/', {
-            'vorname': 'Reiner',
-            'nachname': 'Zufall',
-            'email': 'reiner@z.de',
-            'titel': 'TestantragTEST',
+            'vorname': self.vorname,
+            'nachname': self.nachname,
+            'email': self.email,
+            'titel': self.titel,
             'referat': '1',
             'ist_eilantrag': False,
-            'text': 'Dies ist ein Testantrag',
-            'grund': 'Testgrund',
-            'vorschlag': 'Testvorschlag',
+            'text': self.text,
+            'grund': self.grund,
+            'vorschlag': self.vorschlag,
             'anlagen': ''
         })
         self.assertEqual(response.status_code, 200)
 
         # Überprüfe, ob der Antrag in der Datenbank erstellt wurde
-        antrag = Antrag.objects.filter(antragTitel='TestantragTEST').filter(antragText='Dies ist ein Testantrag').filter(antragVorschlag='Testvorschlag').first()
-        self.assertEqual(antrag.astellerID.astellerVorname, 'Reiner')
-        self.assertEqual(antrag.astellerID.astellerName, 'Zufall')
-        self.assertEqual(antrag.antragText, 'Dies ist ein Testantrag')
-        self.assertEqual(antrag.istEilantrag, False)
-        self.assertEqual(antrag.antragGrund, 'Testgrund')
-        self.assertEqual(antrag.antragVorschlag, 'Testvorschlag')
+        self.assertTrue(
+        Antrag.objects.filter(
+            ).filter(
+                astellerID__astellerVorname=self.vorname
+            ).filter(
+                astellerID__astellerName=self.nachname
+            ).filter(
+                astellerID__astellerEmail=self.email
+            ).filter(
+                antragTitel=self.titel
+            ).filter(
+                antragText=self.text
+            ).filter(
+                antragVorschlag=self.vorschlag
+            ).filter(
+                antragGrund=self.grund
+            ).exists()
+        )
 
+    def test_antrag_allgemein_delete(self):
+        response = self.client.post('/antrag/allgemein/', {
+            'vorname': self.vorname,
+            'nachname': self.nachname,
+            'email': self.email,
+            'titel': self.titel,
+            'referat': '1',
+            'ist_eilantrag': False,
+            'text': self.text,
+            'grund': self.grund,
+            'vorschlag': self.vorschlag,
+            'anlagen': ''
+        })
+        self.assertEqual(response.status_code, 200)
+
+        # Überprüfe, ob der Antrag in der Datenbank erstellt wurde
+        antrag = Antrag.objects.filter(
+            ).filter(
+                astellerID__astellerVorname=self.vorname
+            ).filter(
+                astellerID__astellerName=self.nachname
+            ).filter(
+                astellerID__astellerEmail=self.email
+            ).filter(
+                antragTitel=self.titel
+            ).filter(
+                antragText=self.text
+            ).filter(
+                antragVorschlag=self.vorschlag
+            ).filter(
+                antragGrund=self.grund
+            )
+            
         antrag.delete()
-
-    def test_antrag_delete(self):
-        antrag = Antrag.objects.filter(antragTitel='TestantragTEST').filter(antragText='Dies ist ein Testantrag').filter(antragVorschlag='Testvorschlag').first()
-        self.assertIsNone(antrag)
+        
+        self.assertFalse(antrag.exists())
