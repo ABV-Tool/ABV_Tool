a840d29a262c4d947b053361f9a6a174c052d86c
MaxPtg <max.patecky@htw-dresden.de>
2023-06-17 14:33:21 +0200

    added route protection

--- src/abv/Antragstool/urls.py
+++ src/abv/Antragstool/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.urls import path, re_path, include
 from .views import  LoginPage, LogoutPage
 from .views import HomePage, ArchivPage
@@ -9,29 +9,32 @@ from .views import AntragAllgemein, AntragFinanziell, AntragVeranstaltung, Antra
 
 #TODO: Internen Bereich nur für superuser freigeben
 
+def is_admin(user):
+    return user.is_authenticated and user.is_superuser
+
 urlpatterns = [
     # Hauptseiten
     path('', HomePage, name='index'),
     path('archiv/', ArchivPage, name='archiv'),
     
     # Interner Bereich
-    path('intern/referatsverwaltung/', ReferatsverwaltungPage, name='referatsverwaltung'),
-    path('intern/referat/erstellen', ReferatErstellenPage, name='referat-erstellen'),
-    path('intern/referat/<int:refID>/bearbeiten', ReferatBearbeitenPage, name='referat-bearbeiten'),
-    path('intern/referat/<int:refID>/loeschen', ReferatLoeschenPage, name='referat-loeschen'),
+    path('intern/referatsverwaltung/', login_required(user_passes_test(is_admin)(ReferatsverwaltungPage)), name='referatsverwaltung'),
+    path('intern/referat/erstellen', login_required(user_passes_test(is_admin)(ReferatErstellenPage)), name='referat-erstellen'),
+    path('intern/referat/<int:refID>/bearbeiten', login_required(user_passes_test(is_admin)(ReferatBearbeitenPage)), name='referat-bearbeiten'),
+    path('intern/referat/<int:refID>/loeschen', login_required(user_passes_test(is_admin)(ReferatLoeschenPage)), name='referat-loeschen'),
     
-    path('intern/sitzungsverwaltung/', SitzungsverwaltungPage, name='sitzungsverwaltung'),
-    path('intern/sitzung/anlegen', SitzungAnlegenPage, name='sitzung-anlegen'),
-    path('intern/sitzung/<uuid:sitzID>/anzeigen', SitzungVerwaltenPage, name='sitzung-verwalten'),
-    path('intern/sitzung/<uuid:sitzID>/vertagen', SitzungVertagenPage, name='sitzung-vertagen'),
-    path('intern/sitzung/<uuid:sitzID>/loeschen', SitzungLoeschenPage, name='sitzung-loeschen'),
+    path('intern/sitzungsverwaltung/', login_required(user_passes_test(is_admin)(SitzungsverwaltungPage)), name='sitzungsverwaltung'),
+    path('intern/sitzung/anlegen', login_required(user_passes_test(is_admin)(SitzungAnlegenPage)), name='sitzung-anlegen'),
+    path('intern/sitzung/<uuid:sitzID>/anzeigen', login_required(user_passes_test(is_admin)(SitzungVerwaltenPage)), name='sitzung-verwalten'),
+    path('intern/sitzung/<uuid:sitzID>/vertagen', login_required(user_passes_test(is_admin)(SitzungVertagenPage)), name='sitzung-vertagen'),
+    path('intern/sitzung/<uuid:sitzID>/loeschen', login_required(user_passes_test(is_admin)(SitzungLoeschenPage)), name='sitzung-loeschen'),
     
-    path('intern/antragsverwaltung/', AntragsverwaltungPage, name='antragsverwaltung'),
-    path('intern/antrag/<uuid:antragID>/anzeigen', AntragAnzeigenPage, name='antrag-anzeigen'),
-    path('intern/antrag/<uuid:antragID>/bearbeiten', AntragBearbeitenPage, name='antrag-bearbeiten'),
-    path('intern/antrag/<uuid:antragID>/loeschen', AntragLoeschenPage, name='antrag-loeschen'),
-    path('intern/antrag/<uuid:antragID>/beschliessen', AntragBeschliessenPage, name='antrag-beschliessen'),
-    path('intern/antrag/<uuid:antragID>/vertagen', AntragVertagenPage, name='antrag-vertagen'),
+    path('intern/antragsverwaltung/', login_required(user_passes_test(is_admin)(AntragsverwaltungPage)), name='antragsverwaltung'),
+    path('intern/antrag/<uuid:antragID>/anzeigen', login_required(user_passes_test(is_admin)(AntragAnzeigenPage)), name='antrag-anzeigen'),
+    path('intern/antrag/<uuid:antragID>/bearbeiten', login_required(user_passes_test(is_admin)(AntragBearbeitenPage)), name='antrag-bearbeiten'),
+    path('intern/antrag/<uuid:antragID>/loeschen', login_required(user_passes_test(is_admin)(AntragLoeschenPage)), name='antrag-loeschen'),
+    path('intern/antrag/<uuid:antragID>/beschliessen', login_required(user_passes_test(is_admin)(AntragBeschliessenPage)), name='antrag-beschliessen'),
+    path('intern/antrag/<uuid:antragID>/vertagen', login_required(user_passes_test(is_admin)(AntragVertagenPage)), name='antrag-vertagen'),
     
     # Benutzerverwaltung
     path('accounts/login/', LoginPage, name='login'),
